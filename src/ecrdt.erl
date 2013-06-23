05a2f43bfab639158d98edf3089cf9e1120003bc
Heinz N. Gies <heinz@licenser.net>
2013-06-24 00:51:33 +0200

    Added general functions.

--- src/ecrdt.erl
+++ src/ecrdt.erl
@@ -1,6 +1,8 @@
 -module(ecrdt).
 
--export([id/0]).
+-export([id/0,
+         merge/2,
+         value/1]).
 
 %%%===================================================================
 %%% Implementation
@@ -9,6 +11,30 @@
 -spec id() -> {term(), atom()}.
 id() ->
     {now(), node()}.
+
+merge(A, B) ->
+    M = element(1, A),
+    M = element(1, B),
+    case erlang:function_exported(M, merge, 2) of
+        true ->
+            M:merge(A, B);
+        _ ->
+            erlang:error(badarg)
+    end.
+
+value(A) when is_tupel(A) ->
+    M = element(1, A),
+    case erlang:function_exported(M, value, 1) of
+        true ->
+            M:value(A);
+        _ ->
+            A
+    end;
+
+value(A) ->
+    A.
+
+
 
 %%%===================================================================
 %%% Internal functions
